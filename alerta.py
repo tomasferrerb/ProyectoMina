0c58da53b7f26616835af2bb3bc2c57a4bd94fcf
tomasferrerb <tomas.ferrer.briceno@gmail.com>
2018-09-02 11:40:02 -0300

    alerta.py

--- alerta.py
+++ alerta.py
@@ -17,30 +17,30 @@ def main():
 	#a=0.25*A   #variacion x
 	#b=0.3*B   #variacion y
 	master.configure(background='white')
-	image11 = PhotoImage(file="boton1a.png")
-	image12 = PhotoImage(file="boton1a.png")
-	image13 = PhotoImage(file="boton1a.png")
+	image1a = PhotoImage(file="boton1a.png")
+	image1b = PhotoImage(file="boton1b.png")
+	image1c = PhotoImage(file="boton1c.png")
 
-	scale_w = 1
-	scale_h = 1
-	image11.zoom(scale_w, scale_h)
-	image12.zoom(scale_w, scale_h)
-	image13.zoom(scale_w, scale_h)
+	image2a = PhotoImage(file="boton2a.png")
+	image2b = PhotoImage(file="boton2b.png")
+	image2c = PhotoImage(file="boton2c.png")
+
+
 
 	labelfont= ('arial', 24, 'bold')
 
 
 	l1=Label(master, text="ALERTA DE TORMENTA DE VIENTO",bg="white",font=labelfont).place(relx=0.5,rely=0.1,anchor=CENTER)
-	boton11 = Button(master,bg="white",bd=0,image=image11,command=master.quit).place(relx=0.17, rely=0.3, anchor=CENTER)
-	boton12 = Button(master,bg="white",bd=0,image=image12,command=accion).place(relx=0.5, rely=0.3, anchor=CENTER)
-	boton13 = Button(master,bg="white",bd=0,image=image13 ).place(relx=0.83, rely=0.3, anchor=CENTER)
+	boton1a = Button(master,bg="white",bd=0,image=image1a,command=master.quit).place(relx=0.17, rely=0.3, anchor=CENTER)
+	boton1b = Button(master,bg="white",bd=0,image=image1b,command=accion).place(relx=0.5, rely=0.3, anchor=CENTER)
+	boton1c = Button(master,bg="white",bd=0,image=image1c ).place(relx=0.83, rely=0.3, anchor=CENTER)
 
 
 
 	l2=Label(master,text="ALERTA DE TORMENTA ELÉCTRICA",bg="white",font=labelfont).place(relx=0.5,rely=0.5,anchor=CENTER)
-	boton21 = Button(master,image=image11,bg="white",bd=0,command=master.quit).place(relx=0.17, rely=0.7, anchor=CENTER)
-	boton22 = Button(master, image=image11, bg="white",bd=0,command=accion).place(relx=0.5, rely=0.7, anchor=CENTER)
-	boton23 = Button(master,image=image11,bg="white",bd=0).place(relx=0.83, rely=0.7, anchor=CENTER)
+	boton2a = Button(master,image=image2a,bg="white",bd=0,command=master.quit).place(relx=0.17, rely=0.7, anchor=CENTER)
+	boton2b = Button(master, image=image2b, bg="white",bd=0,command=accion).place(relx=0.5, rely=0.7, anchor=CENTER)
+	boton2c = Button(master,image=image2c,bg="white",bd=0).place(relx=0.83, rely=0.7, anchor=CENTER)
 
 
 	master.mainloop()

67464762f538a6b8e5a0dc5b106a4bc747b86424
tomasferrerb <tomas.ferrer.briceno@gmail.com>
2018-09-07 15:27:55 -0300

    archivo para mostrar en pantalla

---
+++ displayScreen.py
@@ -0,0 +1,47 @@
+from tkinter import *
+from tkinter import messagebox
+import os
+import argparse
+
+
+
+def displayScreen(opt):
+
+	master = Tk()
+	A=master.winfo_screenwidth()
+	B=master.winfo_screenheight()
+	#master.geometry("{0}x{1}+0+0".format(A,B))
+	master.attributes('-fullscreen', True)
+	master.configure(background='white')
+	nombre = "boton" + opt +".png"
+	print(nombre)
+
+###IMAGENES A MOSTRAR###
+	imagen = PhotoImage(file=nombre)
+
+	
+	boton1a = Button(master,bg="white",bd=0,text='X',command=lambda:master.quit()).place(relx=0.1, rely=0.9, anchor=CENTER)
+        
+
+	l1=Label(master, image=imagen,bg="white").place(relx=0.5,rely=0.5,anchor=CENTER)
+
+	master.mainloop()
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='mostrar imagen en pantalla completa',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--imagen', type=str,help='seleccionar imagen, opciones: 1a, 1b, 1c, 2a, 2b, 2c')
+
+
+
+    args = parser.parse_args()
+
+    try:
+        displayScreen(args.imagen)
+    except KeyboardInterrupt:
+        pass
+
---
+++ readSMS-displayScreen.py
@@ -0,0 +1,75 @@
+import serial
+import os, time, sys
+import datetime
+
+
+
+#os.system('vcgencmd display_power 0') #Turn off HDMI signal
+
+
+SERIAL_PORT = "/dev/ttyS0"    # Rasp 3 UART Port
+
+ser = serial.Serial(SERIAL_PORT, baudrate = 9600, timeout = 5)
+#setup()
+##ser.write('AT'+'\r\n')
+##time.sleep(10)
+ser.write('AT+CMGF=1'+'\r\n') # set to text mode
+time.sleep(1)
+ser.write('AT+CMGDA="DEL ALL"\n') # delete all SMS
+time.sleep(1)
+
+#os.system('vcgencmd display_power 0') #Turn off HDMI signal
+def lastPart(str,i):
+    l=str.split('\n',20)
+    return l[i]
+
+def lastPartSet(str,i):
+    l=str.split(',',20)
+    return l[i]
+
+reply = ser.read(ser.inWaiting()) # Clean buf
+print ("Listening for incomming SMS...")
+while True:
+    reply = ser.read(ser.inWaiting())
+    if reply != "":
+
+
+        ser.write("AT+CMGR=1\n") 
+        time.sleep(1)
+        reply = ser.read(ser.inWaiting())
+        print("SMS received. Content:")
+        print(reply)
+##        for p in range(6):
+##            lala=lastPart(reply,p)
+##            print(lala+str(p))
+
+        reply=lastPart(reply,4)
+        msg=lastPartSet(reply,0)
+        clr=lastPartSet(reply,1)
+                        
+        print(msg)
+        print(clr)
+        #os.system('vcgencmd display_power 1') #Turn on HDMI signal
+        time.sleep(1)
+        if '1a' in msg:
+            command='python3 displayScreen --imagen 1a'
+        if '1b' in msg:
+            command='python3 displayScreen --imagen 1b'
+        if '1c' in msg:
+            command='python3 displayScreen --imagen 1c'
+
+        if '2a' in msg:
+            command='python3 displayScreen --imagen 2a'
+        if '2b' in msg:
+            command='python3 displayScreen --imagen 2b'
+        if '2c' in msg:
+            command='python3 displayScreen --imagen 2c'
+        os.system(command)
+
+        
+        time.sleep(.500)
+        ser.write('AT+CMGDA="DEL ALL"\n') # delete all
+        time.sleep(.500)
+        ser.read(ser.inWaiting()) # Clear buffer
+        time.sleep(.500)  
+#os.system('vcgencmd display_power 1') #Turn on HDMI signal no pasa al final

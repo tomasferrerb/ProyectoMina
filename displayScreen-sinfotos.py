394a2ce823e7987531109e5216830bb0868dad95
tomasferrerb <tomas.ferrer.briceno@gmail.com>
2018-10-02 11:53:26 -0300

    pixels

--- displayScreen-sinfotos.py
+++ displayScreen-sinfotos.py
@@ -10,7 +10,7 @@ def displayScreen(opt):
 	master = Tk()
 	A=master.winfo_screenwidth()
 	B=master.winfo_screenheight()
-	master.geometry("96x96+8+150")
+	master.geometry("100x100+8+150")
 	#master.attributes('-fullscreen', True)
 	
 	if 'a' in opt:
